72ec82b68666637d09e54bb4f66d682d231cb9f1
colt <drownworb@gmail.com>
2024-11-02 18:32:03 +0800

    add functionality of the gameboard and player

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,12 @@ import GameBoard from "./components/GameBoard";
 import "./App.css";
 
 function App() {
+  const [activePlayer, setActivePlayer] = useState("X");
+
+  function handleSelectSquare() {
+    setActivePlayer((curActivePlayer) => (curActivePlayer === "X" ? "O" : "X"));
+  }
+
   return (
     <main>
       <header>
@@ -15,11 +21,22 @@ function App() {
         <h1>Tic-Tac-Toe</h1>
       </header>
       <div id="game-container">
-        <ol id="players">
-          <Player initialName="Player 1" symbol="X" />
-          <Player initialName="Player 2" symbol="O" />
+        <ol id="players" className="highlight-player">
+          <Player
+            initialName="Player 1"
+            symbol="X"
+            isActive={activePlayer === "X"}
+          />
+          <Player
+            initialName="Player 2"
+            symbol="O"
+            isActive={activePlayer === "O"}
+          />
         </ol>
-        <GameBoard />
+        <GameBoard
+          onSelectSquare={handleSelectSquare}
+          activePlayerSymbol={activePlayer}
+        />
       </div>
       LOG
     </main>
--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -6,7 +6,7 @@ const initialGameBoard = [
   [null, null, null],
 ];
 
-function GameBoard() {
+function GameBoard({ onSelectSquare, activePlayerSymbol }) {
   const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
   function handleSelectSquare(rowIndex, colIndex) {
@@ -14,9 +14,11 @@ function GameBoard() {
       const updateBoard = [
         ...prevGameBoard.map((innerArray) => [...innerArray]),
       ]; // update a new memory of array of "initialGameBoard"
-      updateBoard[rowIndex][colIndex] = "X";
+      updateBoard[rowIndex][colIndex] = activePlayerSymbol;
       return updateBoard;
     });
+
+    onSelectSquare();
   }
 
   return (
