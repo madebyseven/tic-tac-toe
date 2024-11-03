9e54f55ede23f48867f1ff94b597698e3f49b475
colt <drownworb@gmail.com>
2024-11-03 23:34:57 +0800

    outsourcing data into a separate file

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
 import Log from "./components/Log";
+import { WINNING_COMBINATIONS } from "./components/winning_combinations";
 
 import "./App.css";
 
